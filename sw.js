0c71e5ad9daf16d5e78d84d757b60e3218db0880
Patrickify <lang.patrick@outlook.com>
2026-02-17 23:11:16 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,39 @@
-const CACHE = "aufgaben-zdl-ra93-v1";
-const ASSETS = ["./","./index.html","./styles.css","./app.js","./manifest.json","./icon-192.png","./icon-512.png"];
+const CACHE = "zdl-ra93-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
 
-self.addEventListener("install", (e)=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+self.addEventListener("install", (e) => {
+  e.waitUntil((async () => {
+    const c = await caches.open(CACHE);
+    await c.addAll(ASSETS);
+    self.skipWaiting();
+  })());
 });
-self.addEventListener("activate", (e)=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.map(k => (k !== CACHE) ? caches.delete(k) : null));
+    self.clients.claim();
+  })());
 });
-self.addEventListener("fetch", (e)=>{
-  e.respondWith(
-    caches.match(e.request).then(r=> r || fetch(e.request).then(res=>{
-      const copy = res.clone();
-      caches.open(CACHE).then(c=>c.put(e.request, copy)).catch(()=>{});
-      return res;
-    }).catch(()=>r))
-  );
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith((async () => {
+    const cached = await caches.match(e.request);
+    if (cached) return cached;
+    try {
+      const resp = await fetch(e.request);
+      return resp;
+    } catch {
+      return caches.match("./index.html");
+    }
+  })());
 });
